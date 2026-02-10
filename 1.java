cc185dbaad3df5f31e7a71a85091a636e7af5619
prarthana450 <167793851+prarthana450@users.noreply.github.com>
2026-02-10 10:30:06 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,15 @@
+Write a program to print the numbers from 10 to 50 using for loop/while loop.
+package com.sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=10;i<=50;i++) {
+			System.out.println(-i);
+		}
+		
+	}
+
+}
